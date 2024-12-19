297e48e71edf7d2022202a2833de3e2c1c3c3262
NokulungaMotsweni <56791409+NokulungaMotsweni@users.noreply.github.com>
2024-12-19 20:03:59 +0100

    Update bob.clj
    Create response for silence where the sentence contains nothing or combinations of whitespace characters.

--- bob.clj
+++ bob.clj
@@ -1,1 +1,11 @@
+(ns bob_responses
+  :require [clojure.string :as str]))
 
+(defn response_for 
+  [sentence]
+  (let [trimmed (str/trim sentence)
+        letters-only (filter #(Character/isLetter %) trimmed ]
+
+        (cond 
+          (str/blank? sentence) "Fine. Be that way!"
+          
