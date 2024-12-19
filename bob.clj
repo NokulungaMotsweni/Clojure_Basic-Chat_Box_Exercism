ba86fcbbb33fe66005aa583e1a2040f8bbe047b6
NokulungaMotsweni <56791409+NokulungaMotsweni@users.noreply.github.com>
2024-12-19 20:54:46 +0100

    Update bob.clj
    Changed the order of the conditions and changed the variable named of trimmed to be more specific as it refers to the trimmed sentence.

--- bob.clj
+++ bob.clj
@@ -1,25 +1,29 @@
 (ns bob_responses
   :require [clojure.string :as str]))
 
-(defn response_for 
+(defn response-for 
   [sentence]
-  (let [trimmed (str/trim s)]
+  (let [trimmed_sentence (clojure.string/trim sentence)]
+  
+    (cond 
+      ;; Bob's response to silense or combinations of various whitespace characters.
+      (clojure.string/blank? trimmed_sentence) "Fine. Be that way!"
 
-;; Condition for the various responses that Bob will give.        
-        (cond 
-            ;; Bob's response to silense or combinations of various whitespace characters.
-          (str/blank? sentence) "Fine. Be that way!"
+      ;; Bob's response to a question being yelled at him, where the sentence entails all capitals and a question mark at the end of the sentence.
+      (and (= (last trimmed_sentence) \?)
+           (= trimmed_sentence (str/upper-case trimmed_sentence))
+           (some #(Character/isLetter %) trimmed_sentence)) "Calm down, I know what I'm doing!"
 
-                    ;; Bob's response to a question being yelled at him, where the sentence entails all capitals and a question mark at the end of the sentence.
-        (and (last trimmed) "?")
-            (= trimmed (str/upper-case trimmed))) "Calm down, I know what I'm doing!"
+      ;; Bob's response to anyone yelling at home which is all capital letters within the sentence.
+      (and (= trimmed_sentence (clojure.string/upper-case trimmed_sentence))
+           (some #(Character/isLetter %) trimmed_sentence)) "Whoa, chill out!"
 
-          ;; Bob's response to anyone yelling at home which is all capital letters within the sentence.
-          (and (= trimmed (str/upper-case trimmed))
-                (some #(Character/isLetter %) trimmed)) "Whoa, chill out!"
+     ;; Bob's response to sure where the sentence ends in a question mark.
+      (= (last trimmed_sentence) \?) "Sure."
 
-         ;; Bob's response to sure where the sentence ends in a question mark.
-          (= (last trimmed) \?) "Sure."
-          
-        )))
+      ;; Default response for anything else.
+      :else "Whatever."
+    ))
+  )
+
           
