61ee681647505687cb812f70de59f0201621888f
NokulungaMotsweni <56791409+NokulungaMotsweni@users.noreply.github.com>
2024-12-19 20:14:08 +0100

    Update bob.clj
    Creating the code which will be Bob's response to anyone yelling at home which is all capital letters within the sentence..

--- bob.clj
+++ bob.clj
@@ -6,6 +6,12 @@
   (let [trimmed (str/trim sentence)
         letters-only (filter #(Character/isLetter %) trimmed ]
 
+;; Condition for the various responses that Bob will give.        
         (cond 
+          ;; Bob's response to silense or combinations of various whitespace characters.
           (str/blank? sentence) "Fine. Be that way!"
+
+          ;; Bob's response to anyone yelling at home which is all capital letters within the sentence.
+          (and (= trimmed |(str/upper-case trimmed))
+                (some #(Character/isLetter %) trimmed)) "Whoa, chill out!"
           
