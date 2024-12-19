45167c9f5a38d0d43d43189561ec0bfde5ec9b32
NokulungaMotsweni <56791409+NokulungaMotsweni@users.noreply.github.com>
2024-12-19 20:38:08 +0100

    Update bob.clj
    Changes the let bindings, to be more simpler as the letters-only seems ti be quire restrictive, may use it again if my code doesn't like it.
    Swapped the conditions around as some incorrect responses were generated due to the order.

--- bob.clj
+++ bob.clj
@@ -3,18 +3,23 @@
 
 (defn response_for 
   [sentence]
-  (let [trimmed (str/trim sentence)
-        letters-only (filter #(Character/isLetter %) trimmed ]
+  (let [trimmed (str/trim s)]
 
 ;; Condition for the various responses that Bob will give.        
         (cond 
-          ;; Bob's response to sure where the sentence ends in a question mark.
-          (= (last trimmed) \?) "Sure."
-          
-          ;; Bob's response to silense or combinations of various whitespace characters.
+            ;; Bob's response to silense or combinations of various whitespace characters.
           (str/blank? sentence) "Fine. Be that way!"
+
+                    ;; Bob's response to a question being yelled at him, where the sentence entails all capitals and a question mark at the end of the sentence.
+        (and (last trimmed) "?")
+            (= trimmed (str/upper-case trimmed))) "Calm down, I know what I'm doing!"
 
           ;; Bob's response to anyone yelling at home which is all capital letters within the sentence.
           (and (= trimmed |(str/upper-case trimmed))
                 (some #(Character/isLetter %) trimmed)) "Whoa, chill out!"
+
+         ;; Bob's response to sure where the sentence ends in a question mark.
+          (= (last trimmed) \?) "Sure."
+          
+        )))
           
